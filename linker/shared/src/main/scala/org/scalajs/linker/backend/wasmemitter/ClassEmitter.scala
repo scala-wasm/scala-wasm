scala-js/scala-js

Wasm: Implement checked asInstanceOfs.

linker/shared/src/main/scala/org/scalajs/linker/backend/wasmemitter/ClassEmitter.scala
@@ -20,6 +20,7 @@ import org.scalajs.ir.OriginalName.NoOriginalName
 import org.scalajs.ir.Trees._
 import org.scalajs.ir.Types._
 
+import org.scalajs.linker.interface.CheckedBehavior
 import org.scalajs.linker.interface.unstable.RuntimeClassNameMapperImpl
 import org.scalajs.linker.standard.{CoreSpec, LinkedClass, LinkedTopLevelExport}
 
@@ -37,6 +38,7 @@ import WasmContext._
 
 class ClassEmitter(coreSpec: CoreSpec) {
   import ClassEmitter._
+  import coreSpec.semantics
 
   def genClassDef(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
     val classInfo = ctx.getClassInfo(clazz.className)
@@ -347,6 +349,12 @@ class ClassEmitter(coreSpec: CoreSpec) {
         genCloneFunction(clazz)
     }
 
+    // Generate cast functions
+    if (clazz.hasInstanceTests && semantics.asInstanceOfs != CheckedBehavior.Unchecked) {
+      if (className != ObjectClass)
+        genClassCastFunction(clazz)
+    }
+
     // Generate the module accessor
     if (clazz.kind == ClassKind.ModuleClass && clazz.hasInstances) {
       val heapType = watpe.HeapType(genTypeID.forClass(clazz.className))
@@ -382,7 +390,7 @@ class ClassEmitter(coreSpec: CoreSpec) {
       case None    => genTypeID.typeData
       case Some(s) => genTypeID.forVTable(s.name)
     }
-    val structType = watpe.StructType(CoreWasmLib.typeDataStructFields ::: vtableFields)
+    val structType = watpe.StructType(ctx.coreLib.typeDataStructFields ::: vtableFields)
     val subType = watpe.SubType(
       typeID,
       makeOriginalName(ns.VTable, className),
@@ -488,6 +496,50 @@ class ClassEmitter(coreSpec: CoreSpec) {
     fb.buildAndAddToModule()
   }
 
+  /** Generate type inclusion test for interfaces.
+   *
+   *  The expression `isInstanceOf[<interface>]` will be compiled to a CALL to the function
+   *  generated by this method.
+   */
+  private def genInterfaceCastFunction(clazz: LinkedClass)(
+      implicit ctx: WasmContext): Unit = {
+    assert(clazz.kind == ClassKind.Interface)
+
+    val className = clazz.className
+    val resultType = TypeTransformer.transformClassType(className)
+
+    val fb = new FunctionBuilder(
+      ctx.moduleBuilder,
+      genFunctionID.asInstance(ClassType(className)),
+      makeOriginalName(ns.AsInstance, className),
+      clazz.pos
+    )
+    val objParam = fb.addParam("obj", watpe.RefType.anyref)
+    fb.setResultType(resultType)
+
+    fb.block() { successLabel =>
+      // Succeed if null
+      fb += wa.LocalGet(objParam)
+      fb += wa.BrOnNull(successLabel)
+
+      // Succeed if the instance test succeeds
+      fb += wa.Call(genFunctionID.instanceTest(className))
+      fb += wa.BrIf(successLabel)
+
+      // If we get here, it's a CCE
+      fb += wa.LocalGet(objParam)
+      fb += wa.GlobalGet(genGlobalID.forVTable(className))
+      fb += wa.Call(genFunctionID.classCastException)
+      fb += wa.Unreachable
+    }
+
+    fb += wa.LocalGet(objParam)
+    if (resultType != watpe.RefType.anyref)
+      fb += wa.RefCast(resultType)
+
+    fb.buildAndAddToModule()
+  }
+
   private def genNewDefaultFunc(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
     val className = clazz.name.name
     val classInfo = ctx.getClassInfo(className)
@@ -560,6 +612,51 @@ class ClassEmitter(coreSpec: CoreSpec) {
 
     // Create the result
     fb += wa.StructNew(structTypeID)
+
+    fb.buildAndAddToModule()
+  }
+
+  private def genClassCastFunction(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
+    val className = clazz.className
+
+    val resultType = TypeTransformer.transformClassType(className)
+
+    val fb = new FunctionBuilder(
+      ctx.moduleBuilder,
+      genFunctionID.asInstance(ClassType(clazz.className)),
+      makeOriginalName(ns.AsInstance, className),
+      clazz.pos
+    )
+    val objParam = fb.addParam("obj", watpe.RefType.anyref)
+    fb.setResultType(resultType)
+
+    fb.block(resultType) { successLabel =>
+      fb += wa.LocalGet(objParam)
+
+      if (className == SpecialNames.JLNumberClass) {
+        /* jl.Number is special, because it is the only non-Object *class*
+         * that is an  ancestor of a hijacked class.
+         */
+        fb += wa.BrOnCast(successLabel, watpe.RefType.anyref,
+            watpe.RefType.nullable(genTypeID.forClass(SpecialNames.JLNumberClass)))
+
+        /* The `obj` still on the stack will be used for:
+         * a) the result in the true case
+         * b) consistency with non-Number in the false case
+         */
+
+        fb += wa.LocalGet(objParam)
+        fb += wa.Call(genFunctionID.typeTest(DoubleRef))
+        fb += wa.BrIf(successLabel)
+      } else {
+        fb += wa.BrOnCast(successLabel, watpe.RefType.anyref, resultType)
+      }
+
+      // If we get here, it's a CCE -- `obj` is still on the stack
+      fb += wa.GlobalGet(genGlobalID.forVTable(className))
+      fb += wa.Call(genFunctionID.classCastException)
+      fb += wa.Unreachable
+    }
 
     fb.buildAndAddToModule()
   }
@@ -651,8 +748,11 @@ class ClassEmitter(coreSpec: CoreSpec) {
       itableType
     )
 
-    if (clazz.hasInstanceTests)
+    if (clazz.hasInstanceTests) {
       genInterfaceInstanceTest(clazz)
+      if (semantics.asInstanceOfs != CheckedBehavior.Unchecked)
+        genInterfaceCastFunction(clazz)
+    }
   }
 
   private def genJSClass(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
@@ -1221,6 +1321,7 @@ object ClassEmitter {
     val JSClassValueCache = UTF8String("b.")
     val TypeData = UTF8String("d.")
     val IsInstance = UTF8String("is.")
+    val AsInstance = UTF8String("as.")
 
     // Shared with JS backend -- string
     val TopLevelExport = UTF8String("e.")

scala-wasm/scala-wasm

linker/shared/src/main/scala/org/scalajs/linker/backend/wasmemitter/ClassEmitter.scala
@@ -20,6 +20,7 @@ import org.scalajs.ir.OriginalName.NoOriginalName
 import org.scalajs.ir.Trees._
 import org.scalajs.ir.Types._
 
+import org.scalajs.linker.interface.CheckedBehavior
 import org.scalajs.linker.interface.unstable.RuntimeClassNameMapperImpl
 import org.scalajs.linker.standard.{CoreSpec, LinkedClass, LinkedTopLevelExport}
 
@@ -37,6 +38,7 @@ import WasmContext._
 
 class ClassEmitter(coreSpec: CoreSpec) {
   import ClassEmitter._
+  import coreSpec.semantics
 
   def genClassDef(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
     val classInfo = ctx.getClassInfo(clazz.className)
@@ -373,6 +375,12 @@ class ClassEmitter(coreSpec: CoreSpec) {
         genCloneFunction(clazz)
     }
 
+    // Generate cast functions
+    if (clazz.hasInstanceTests && semantics.asInstanceOfs != CheckedBehavior.Unchecked) {
+      if (className != ObjectClass)
+        genClassCastFunction(clazz)
+    }
+
     // Generate the module accessor
     if (clazz.kind == ClassKind.ModuleClass && clazz.hasInstances) {
       val heapType = watpe.HeapType(genTypeID.forClass(clazz.className))
@@ -408,7 +416,7 @@ class ClassEmitter(coreSpec: CoreSpec) {
       case None    => genTypeID.typeData
       case Some(s) => genTypeID.forVTable(s.name)
     }
-    val structType = watpe.StructType(CoreWasmLib.typeDataStructFields ::: vtableFields)
+    val structType = watpe.StructType(ctx.coreLib.typeDataStructFields ::: vtableFields)
     val subType = watpe.SubType(
       typeID,
       makeDebugName(ns.VTable, className),
@@ -522,6 +530,50 @@ class ClassEmitter(coreSpec: CoreSpec) {
     fb.buildAndAddToModule()
   }
 
+  /** Generate the cast function for an interface.
+   *
+   *  When `asInstanceOfs` are checked, the expression `asInstanceOf[<interface>]`
+   *  will be compiled to a CALL to the function generated by this method.
+   */
+  private def genInterfaceCastFunction(clazz: LinkedClass)(
+      implicit ctx: WasmContext): Unit = {
+    assert(clazz.kind == ClassKind.Interface)
+
+    val className = clazz.className
+    val resultType = TypeTransformer.transformClassType(className, nullable = true)
+
+    val fb = new FunctionBuilder(
+      ctx.moduleBuilder,
+      genFunctionID.asInstance(ClassType(className, nullable = true)),
+      makeDebugName(ns.AsInstance, className),
+      clazz.pos
+    )
+    val objParam = fb.addParam("obj", watpe.RefType.anyref)
+    fb.setResultType(resultType)
+
+    fb.block() { successLabel =>
+      // Succeed if null
+      fb += wa.LocalGet(objParam)
+      fb += wa.BrOnNull(successLabel)
+
+      // Succeed if the instance test succeeds
+      fb += wa.Call(genFunctionID.instanceTest(className))
+      fb += wa.BrIf(successLabel)
+
+      // If we get here, it's a CCE
+      fb += wa.LocalGet(objParam)
+      fb += wa.GlobalGet(genGlobalID.forVTable(className))
+      fb += wa.Call(genFunctionID.classCastException)
+      fb += wa.Unreachable
+    }
+
+    fb += wa.LocalGet(objParam)
+    if (resultType != watpe.RefType.anyref)
+      fb += wa.RefCast(resultType)
+
+    fb.buildAndAddToModule()
+  }
+
   private def genNewDefaultFunc(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
     val className = clazz.name.name
     val classInfo = ctx.getClassInfo(className)
@@ -594,6 +646,56 @@ class ClassEmitter(coreSpec: CoreSpec) {
 
     // Create the result
     fb += wa.StructNew(structTypeID)
+
+    fb.buildAndAddToModule()
+  }
+
+  /** Generate the cast function for a class.
+   *
+   *  When `asInstanceOfs` are checked, the expression `asInstanceOf[<class>]`
+   *  will be compiled to a CALL to the function generated by this method.
+   */
+  private def genClassCastFunction(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
+    val className = clazz.className
+
+    val resultType = TypeTransformer.transformClassType(className, nullable = true)
+
+    val fb = new FunctionBuilder(
+      ctx.moduleBuilder,
+      genFunctionID.asInstance(ClassType(clazz.className, nullable = true)),
+      makeDebugName(ns.AsInstance, className),
+      clazz.pos
+    )
+    val objParam = fb.addParam("obj", watpe.RefType.anyref)
+    fb.setResultType(resultType)
+
+    fb.block(resultType) { successLabel =>
+      fb += wa.LocalGet(objParam)
+
+      if (className == SpecialNames.JLNumberClass) {
+        /* jl.Number is special, because it is the only non-Object *class*
+         * that is an  ancestor of a hijacked class.
+         */
+        fb += wa.BrOnCast(successLabel, watpe.RefType.anyref,
+            watpe.RefType.nullable(genTypeID.forClass(SpecialNames.JLNumberClass)))
+
+        /* The `obj` still on the stack will be used for:
+         * a) the result in the true case
+         * b) consistency with non-Number in the false case
+         */
+
+        fb += wa.LocalGet(objParam)
+        fb += wa.Call(genFunctionID.typeTest(DoubleRef))
+        fb += wa.BrIf(successLabel)
+      } else {
+        fb += wa.BrOnCast(successLabel, watpe.RefType.anyref, resultType)
+      }
+
+      // If we get here, it's a CCE -- `obj` is still on the stack
+      fb += wa.GlobalGet(genGlobalID.forVTable(className))
+      fb += wa.Call(genFunctionID.classCastException)
+      fb += wa.Unreachable
+    }
 
     fb.buildAndAddToModule()
   }
@@ -685,8 +787,11 @@ class ClassEmitter(coreSpec: CoreSpec) {
       itableType
     )
 
-    if (clazz.hasInstanceTests)
+    if (clazz.hasInstanceTests) {
       genInterfaceInstanceTest(clazz)
+      if (semantics.asInstanceOfs != CheckedBehavior.Unchecked)
+        genInterfaceCastFunction(clazz)
+    }
   }
 
   private def genJSClass(clazz: LinkedClass)(implicit ctx: WasmContext): Unit = {
@@ -1262,6 +1367,7 @@ object ClassEmitter {
     val JSClassValueCache = UTF8String("b.")
     val TypeData = UTF8String("d.")
     val IsInstance = UTF8String("is.")
+    val AsInstance = UTF8String("as.")
 
     // Shared with JS backend -- string
     val TopLevelExport = UTF8String("e.")