af20946bda96f5144f37537fd0c3e0a19ad9a2e0
Rikito Taniguchi <rikiriki1238@gmail.com>
2026-02-20 21:09:01 +0900

    Version 1.20.2-wasm.1-SNAPSHOT

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-      current = "1.21.0-SNAPSHOT",
+      current = "1.20.2-wasm.1-SNAPSHOT",
       binaryEmitted = "1.20"
     )
 
